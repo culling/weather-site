7d5e77aae034e7dd6dc6cac0d4373b9c2a829d54
culling <culling66@gmail.com>
2017-07-20 18:51:09 +0800

    heroku

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express     = require('./config/express');
 
 var app         = express();
-
-var port = 80;
+var config      = require('./config/config');
+var port = config.port;
 app.listen(port);
 module.exports = app;
 console.log('server running at http://localhost:' + port);
